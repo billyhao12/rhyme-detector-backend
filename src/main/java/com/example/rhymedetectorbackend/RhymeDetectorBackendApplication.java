2229a8a178c85b2a1b4b4fa4ec3b6ae6e4dd98b7
Billy Hao <billyhao12@gmail.com>
2024-09-29 16:29:50 -0700

    Initial commit

---
+++ src/main/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhymedetectorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhymeDetectorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhymeDetectorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rhymedetectorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhymeDetectorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
