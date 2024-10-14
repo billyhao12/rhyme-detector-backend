1ad9ae0abdd8ec801fd44c7dd945efdeecd08515
Billy Hao <billyhao12@gmail.com>
2024-10-13 17:58:10 -0700

    testing: assert controller is not null

--- src/test/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplicationTests.java
+++ src/test/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.rhymedetectorbackend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RhymeDetectorBackendApplicationTests {
 
+    @Autowired
+    private Controller controller;
+
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
-
 }
