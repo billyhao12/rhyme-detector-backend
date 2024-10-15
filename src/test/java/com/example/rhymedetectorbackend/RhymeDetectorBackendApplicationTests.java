b48c509407c2e3a038bda553bc63f8b33c2844c3
Billy Hao <billyhao12@gmail.com>
2024-10-15 11:36:19 -0700

    Test hello world endpoint

--- src/test/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplicationTests.java
+++ src/test/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplicationTests.java
@@ -5,15 +5,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RhymeDetectorBackendApplicationTests {
 
     @Autowired
     private Controller controller;
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     void contextLoads() {
         assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void shouldSayHello() throws Exception {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/hello", String.class)).isEqualTo("Hello World!");
     }
 }
