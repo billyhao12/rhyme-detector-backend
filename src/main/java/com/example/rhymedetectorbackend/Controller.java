68feb8408009829758133c0006b10e96d6b75230
Billy Hao <billyhao12@gmail.com>
2024-10-23 08:56:33 -0700

    update @CrossOrigin annotation at controller level

--- src/main/java/com/example/rhymedetectorbackend/Controller.java
+++ src/main/java/com/example/rhymedetectorbackend/Controller.java
@@ -1,13 +1,11 @@
 package com.example.rhymedetectorbackend;
 
 import com.example.rhymedetectorbackend.utils.*;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class Controller {
 
