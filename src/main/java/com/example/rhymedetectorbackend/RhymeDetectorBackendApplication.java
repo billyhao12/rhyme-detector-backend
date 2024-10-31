45d98d09c58b7204690441928eb0fbe2b0fc0f72
Billy Hao <billyhao12@gmail.com>
2024-10-31 15:47:57 -0700

    Add swagger example for basic greeting endpoint

--- src/main/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplication.java
+++ src/main/java/com/example/rhymedetectorbackend/RhymeDetectorBackendApplication.java
@@ -5,6 +5,11 @@
 
 package com.example.rhymedetectorbackend;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +24,32 @@ public class RhymeDetectorBackendApplication {
         SpringApplication.run(RhymeDetectorBackendApplication.class, args);
     }
 
+    @Operation(
+            description = "Basic greeting endpoint",
+            parameters = {
+                    @Parameter(
+                            name = "myName",
+                            description = "The name of the person to greet",
+                            example = "Alice"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful greeting message",
+                            content = @Content(
+                                    mediaType = "text/plain",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "Hello Alice!"
+                                            )
+                                    }
+                            )
+                    )
+            }
+    )
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-
 }
